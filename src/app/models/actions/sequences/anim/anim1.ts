07d92220c5ad4edf6e2569a8266a1f7e0e36cb20
Mark Rudolph <mark_rudolph@yahoo.com>
2021-04-01 20:18:43 -0700

    adds non-trivial actions sequence

--- src/app/models/actions/sequences/anim/anim1.ts
+++ src/app/models/actions/sequences/anim/anim1.ts
@@ -18,37 +18,37 @@ export const actions:Action[] = [
 //        ms:2000
 //       }
 
-//       {t:'vrcsphere',
-//        f:'rotateZ',
-//        a:'n',
-//        o: {'arg':0.785},
-//        ms:2000
-//       },
-//
-//       {t:'vrcsphere',
-//        f:'translateX',
-//        a:'n',
-//        o: {'arg':0.785},
-//        ms:4000
-//       },
+       {t:'vrcsphere',
+        f:'rotateZ',
+        a:'n',
+        o: {'arg':0.785},
+        ms:2000
+       },
+
+       {t:'vrcsphere',
+        f:'translateX',
+        a:'n',
+        o: {'arg':0.785},
+        ms:4000
+       },
 
 
-//       {t:'animation',
-//        f:'perform',
-//        ms:10000,
-//        a:'o',
-//        o: {
-//          timeline:{
-//          },
-//          actors:{
-//            'vrcsphere~material':{
-//              'opacity':0.0,
-//              duration:5,
-//              repeat:-1,
-//              yoyo:true
-//            }
-//
-//          }
-//        }//anim
-//       }//animation.perform
+       {t:'animation',
+        f:'perform',
+        ms:10000,
+        a:'o',
+        o: {
+          timeline:{
+          },
+          actors:{
+            'vrcsphere~material':{
+              'opacity':0.0,
+              duration:5,
+              repeat:-1,
+              yoyo:true
+            }
+
+          }
+        }//anim
+       }//animation.perform
 ];
