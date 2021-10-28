cafa7e5850804f06af3cfa637447a63cbac6410f
Mark Rudolph <mark_rudolph@yahoo.com>
2021-10-28 15:48:22 -0700

    removed directory @TODO

--- @TODO/models/camera/controls/Controls.interface.ts
+++
@@ -1,9 +0,0 @@
-// Controls.interface.ts
-// NOTE: defines an interface which all controls MUST implement,
-// which is 'start' method to initiate mouse-listening
-
-export interface Controls{
-  start(controlTarget:THREE.Object3D,    // object to affect by key-actions
-        domElement:HTMLElement,          // DOM element mouse area - 'canvas'
-        speed?:number):void;             // default 0.1
-}
--- @TODO/models/stage/shaders/dome-vr3-webgl1/vertex/vsh_default.glsl.ts
+++
@@ -1,9 +0,0 @@
-// webGL Vertex shader program 
-// vsh_default - model coords to perspective-view coords - allows model sharing
-
-export var vsh:string = `
-
-  void main(){
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
-  }
-`;
